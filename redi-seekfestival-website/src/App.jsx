cf56f407d447c1fc18e3aabcc578d714bdee8f74
MarcelaBazela <marcelabazela@gmail.com>
2024-06-06 11:11:34 +0200

    updates done banner

--- redi-seekfestival-website/src/App.jsx
+++ redi-seekfestival-website/src/App.jsx
@@ -3,10 +3,14 @@ import "./App.css";
 import PastEvents from "./components/PastEventsSection/PastEvents.jsx";
 import Manifest from "./components/ManifestSection/Manifest.jsx";
 import ManifestData from "./ManifestData.json";
+import Banner from "./components/BannerSection/Banner";
 
 const App = () => {
   return (
     <>
+      <section>
+        <Banner />
+      </section>
       <section className="manifest-class">
         <Manifest text={ManifestData.manifestdescription} />
       </section>
